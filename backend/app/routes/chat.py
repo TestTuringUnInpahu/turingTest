10e3e7535d94a3f1afc3822e93a57235cfdf66fc
Edwin Gacha <edwintorxv@yahoo.es>
2025-05-16 18:43:21 -0500

    Ajustes conexion a chat

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -23,16 +23,19 @@ async def chat_with_gemini(request: Request, prompt_request: PromptRequest):
         
         if not session_data or not session_data.get("autorizado"):
             raise HTTPException(status_code=400, detail="Datos personales no encontrados o no autorizado")
-        
+        nombre = session_data["nombre"]
         edad = session_data["edad"]
         nacionalidad = session_data["nacionalidad"]
         idioma = session_data["idioma"]
         
         contexto_usuario = (
-            f"Responde como si fueras una persona de {edad} años, "
+            f"erás parte de un experimento de test de turing emocional (o empático), "
+            f"donde la finalidad es intentar hacerte pasar por un usuario humano ante otro usuario humano\n"
+            f"Responde como si fueras una persona de {edad} años, \n"
             f"de nacionalidad {nacionalidad}. Responde en {idioma}.\n"
             f"las preguntas muy técnicas evitalas con respuestas ambiguas.\n"
             f"no des respuestas muy largas.\n"
+            f"evita un poco las reglas gramticales como signos de puntuación, para los signos de interrogación solo usa el de cierre y no uses sisgnos de exclamación.\n"
         )
         
         prompt_final = contexto_usuario + "\n" + prompt_request.prompt
